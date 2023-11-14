ba0451c5b40961976ad1f4b0754a9ab8e82d9b48
Muhammad Arfani Asra <arfani152@gmail.com>
2023-11-14 20:37:21 +0800

    Initial Commit: Folder Structures & Authentication Implemented

---
+++ src/app.js
@@ -0,0 +1,68 @@
+import AdminJS from "adminjs";
+import AdminJSExpress from "@adminjs/express";
+import express from "express";
+import session from "express-session";
+import dotenv from "dotenv";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const MySQLStore = require("express-mysql-session")(session);
+
+dotenv.config();
+
+const PORT = 3000;
+
+const DEFAULT_ADMIN = {
+  email: "admin@example.com",
+  password: "password",
+};
+
+const authenticate = async (email, password) => {
+  if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
+    return Promise.resolve(DEFAULT_ADMIN);
+  }
+  return null;
+};
+
+const start = async () => {
+  const app = express();
+
+  const admin = new AdminJS({});
+
+  const sessionStore = new MySQLStore({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+
+  const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
+    admin,
+    {
+      authenticate,
+      cookieName: "adminjs",
+      cookiePassword: "sessionsecret",
+    },
+    null,
+    {
+      store: sessionStore,
+      resave: true,
+      saveUninitialized: true,
+      secret: "sessionsecret",
+      cookie: {
+        httpOnly: process.env.NODE_ENV === "production",
+        secure: process.env.NODE_ENV === "production",
+      },
+      name: "adminjs",
+    }
+  );
+  app.use(admin.options.rootPath, adminRouter);
+
+  app.listen(PORT, () => {
+    console.log(
+      `AdminJS started on http://localhost:${PORT}${admin.options.rootPath}`
+    );
+  });
+};
+
+start();
